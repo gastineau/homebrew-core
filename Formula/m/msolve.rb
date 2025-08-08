635952b34078cfb3e1678900e86a97e0ed9ee062
gastineau <gastineau@imcce.fr>
2025-08-08 15:41:01 +0200

    msolve 0.9.0 (new formula)

---
+++ Formula/m/msolve.rb
@@ -0,0 +1,61 @@
+class Msolve < Formula
+  desc "Library for Polynomial System Solving through Algebraic Methods"
+  homepage "https://msolve.lip6.fr"
+  url "https://github.com/algebraic-solving/msolve/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "742e84cf4d11eeadf62002623ecb7658e5d6d8c838fcf571fac06acf44252983"
+  license "GPL-2.0"
+
+  head "https://gitlab.lip6.fr/safey/msolve.git"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  depends_on "flint"
+  depends_on "gmp"
+  depends_on "mpfr"
+  on_macos do
+    depends_on "libomp"
+  end
+
+  def install
+    if OS.mac?
+      libomp = Formula["libomp"]
+      ENV["OPENMP_CFLAGS"] = "-Xpreprocessor -fopenmp -I#{libomp.opt_include} #{libomp.opt_lib}/libomp.dylib"
+    end
+
+    inreplace "configure.ac", /AX_EXT/, " "
+    system "autoreconf", "-vif"
+    system "./configure",
+           "--disable-dependency-tracking",
+           "--disable-silent-rules",
+           "--enable-openmp=yes",
+           "--prefix=#{prefix}",
+           "--libdir=#{lib}",
+           *std_configure_args 
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"eco10-31.ms").write <<-EOS
+      x0,x1,x2,x3,x4,x5,x6,x7,x8,x9
+      1073741827
+      x0*x1*x9+x1*x2*x9+x2*x3*x9+x3*x4*x9+x4*x5*x9+x5*x6*x9+x6*x7*x9+x7*x8*x9+x0*x9-1,
+      x0*x2*x9+x1*x3*x9+x2*x4*x9+x3*x5*x9+x4*x6*x9+x5*x7*x9+x6*x8*x9+x1*x9-2,
+      x0*x3*x9+x1*x4*x9+x2*x5*x9+x3*x6*x9+x4*x7*x9+x5*x8*x9+x2*x9-3,
+      x0*x4*x9+x1*x5*x9+x2*x6*x9+x3*x7*x9+x4*x8*x9+x3*x9-4,
+      x0*x5*x9+x1*x6*x9+x2*x7*x9+x3*x8*x9+x4*x9-5,
+      x0*x6*x9+x1*x7*x9+x2*x8*x9+x5*x9-6,
+      x0*x7*x9+x1*x8*x9+x6*x9-7,
+      x0*x8*x9+x7*x9-8,
+      x8*x9-9,
+      x0+x1+x2+x3+x4+x5+x6+x7+x8+1
+    EOS
+    system "bin/msolve", "-f", "eco10-31.ms"
+  end
+end
